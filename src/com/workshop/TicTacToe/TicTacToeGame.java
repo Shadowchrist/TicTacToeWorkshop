8d0071a68c4308040353089cc0d8ca1c9b70ec93
Shadowchrist <sbhm.prasad@gmail.com>
2020-10-05 11:12:41 +0530

    UC3 committed

--- src/com/workshop/TicTacToe/TicTacToeGame.java
+++ src/com/workshop/TicTacToe/TicTacToeGame.java
@@ -1,32 +1,48 @@
-package com.workshop.TicTacToe;
+package com.workshop.tictactoe;
 
 import java.util.Arrays;
 import java.util.Scanner;
 
 public class TicTacToeGame {
 
-	private char board[];
-	private void board()
-	{
-		board=new char[10];
-		Arrays.fill(board, ' ');
+	private char[] board;
+	@SuppressWarnings("unused")
+	private static char userSelection;
+	@SuppressWarnings("unused")
+	private static char computerSelection;
+
+	private char[] board() {
+		board = new char[10];
+		Arrays.fill(board, 'X');
+		return board;
 	}
-	
-	public static char choice()
-	{
+
+	public static char choice() {
 		System.out.println("Enter X or O");
 		@SuppressWarnings("resource")
-		char charChoice=(new Scanner(System.in)).next().charAt(0);
+		char charChoice = (new Scanner(System.in)).next().charAt(0);
+		TicTacToeGame.userSelection = charChoice;
+		TicTacToeGame.computerSelection = (charChoice == 'X') ? 'O' : 'X';
 		return charChoice;
 	}
-	
-	public static void main(String args[])
-	{
-		TicTacToeGame newGame=new TicTacToeGame();
-		newGame.board();
-		char selection= choice();
-		switch(selection)
-		{
+
+	public static void displayCurrentBoard(char board[]) {
+		for (int i = 1; i < 10; i++) {
+			if(i%3!=0)
+				System.out.print(" " + board[i] + " |");
+			else
+				System.out.print(" " + board[i] + " ");
+			if (i % 3 == 0 && i != 9) {
+				System.out.println("\n-----------");
+			}
+		}
+	}
+
+	public static void main(String args[]) {
+		TicTacToeGame newGame = new TicTacToeGame();
+		char[] board = newGame.board();
+		char selection = choice();
+		switch (selection) {
 		case 'X':
 			System.out.println("X is assigned to the player");
 			System.out.println("O is assigned to the computer");
@@ -38,5 +54,7 @@ public class TicTacToeGame {
 		default:
 			System.out.println("Wrong Choice!");
 		}
+
+		displayCurrentBoard(board);
 	}
-}	
+}

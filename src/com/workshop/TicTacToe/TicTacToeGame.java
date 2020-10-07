77285a4f1b0ff21ecbea96c69cc7d1e964e75550
Shadowchrist <sbhm.prasad@gmail.com>
2020-10-07 07:34:26 +0530

    UC7 committed

--- src/com/workshop/TicTacToe/TicTacToeGame.java
+++ src/com/workshop/TicTacToe/TicTacToeGame.java
@@ -8,10 +8,14 @@ public class TicTacToeGame {
 	private char[] board;
 	private static char userSelection;
 	private static char computerSelection;
+	private static boolean flag;
+	private static boolean turn;
 	private static Scanner input = new Scanner(System.in);
 
 	private char[] board() {
 		board = new char[10];
+		flag=true;
+		turn=true;
 		Arrays.fill(board, ' ');
 		return board;
 	}
@@ -42,7 +46,7 @@ public class TicTacToeGame {
 			else
 				System.out.print(" " + board[i] + " ");
 			if (i % 3 == 0 && i != 9) {
-				System.out.println("\n-----------");
+				System.out.println("\n---|---|---");
 			}
 		}
 		System.out.println("\n");
@@ -65,26 +69,115 @@ public class TicTacToeGame {
 		else
 			return false;
 	}
+	
+	public static boolean checkIfEqual(char[] board, int pos1, int pos2, int pos3, char playerSymbol)
+	{
+		if((board[pos1]==playerSymbol)&&(board[pos1]==board[pos2])&&(board[pos2]==board[pos3]))
+			return true;
+		return false;
+	}
 
-	public static void takeUserInput(char[] board) {
-		System.out.println("Enter choice: \n");
-		int choice = input.nextInt();
-		if (isPlaceAvailable(board, choice))
-			board[choice] = TicTacToeGame.userSelection;
+	public static int checkBoardConditions(char[] board, char playerSymbol)
+	{
+		if(board.length!=9)
+		{	
+			if((checkIfEqual(board, 1,2,3,playerSymbol))||(checkIfEqual(board, 4,5,6,playerSymbol))||(checkIfEqual(board, 7,8,9,playerSymbol))||
+			  (checkIfEqual(board, 1,4,7,playerSymbol))||(checkIfEqual(board, 2,5,8,playerSymbol))||(checkIfEqual(board, 3,6,9,playerSymbol))||
+			  (checkIfEqual(board, 1,5,9,playerSymbol))||(checkIfEqual(board, 3,5,7,playerSymbol)))
+					
+				{
+					flag=false;
+					return 2;
+				}
+			else
+				return 0;
+		}	
 		else
-			System.out.println("Place not available!");
+		{
+			flag=false;
+			return 1;
+		}
+	}
+	
+	public static void checkResult(int result, char playerSymbol)
+	{
+		if(result==2)
+			System.out.println(playerSymbol + " is the winner \n Game Over!!!");
+		else if(result==1)
+			System.out.println("It's a Tie.");
+		else
+			turn=!turn;
+	}
+	
+	public static void takeUserInput(char[] board) {
+		int check=0;
+		while(check==0)
+		{
+			System.out.println("Enter choice: \n");
+			int choice = input.nextInt();
+			if (isPlaceAvailable(board, choice))
+			{	
+				board[choice] = userSelection;
+				check=1;
+			}
+			else
+				System.out.println(choice+": place not available!");
+		}
+		displayCurrentBoard(board);
+	}
+	
+	public static void takeComputerInput(char[] board)
+	{
+		int check=0;
+		while(check==0)
+		{
+			int position=1+(int)(Math.random()*9);
+			if (isPlaceAvailable(board, position))
+			{	
+				board[position] = computerSelection;
+				check=1;
+			}
+			else
+				System.out.println(position+": place not available!");
+		}
 		displayCurrentBoard(board);
 	}
 
 	public static void main(String args[]) {
 		TicTacToeGame newGame = new TicTacToeGame();
 		char[] board = newGame.board();
-		if (coinToss()) {
+		if (coinToss())
+		{	
 			System.out.println("You win!");
 			choice();
-			displayCurrentBoard(board);
-			takeUserInput(board);
-		} else
+		}
+		else
+		{	
+			computerSelection='X';
+			userSelection='O';
 			System.out.println("You lose! Computer gets the first turn.");
-	}
+			turn=false;
+		}	
+		
+		while(flag)
+		{
+			if(turn)
+			{
+				System.out.println("User's Turn.");
+				displayCurrentBoard(board);
+				takeUserInput(board);
+				int result=checkBoardConditions(board,userSelection);
+				checkResult(result,userSelection);
+			}
+			else
+			{
+				System.out.println("Computer's turn... Please wait...");
+				takeComputerInput(board);
+				int result=checkBoardConditions(board,computerSelection);
+				checkResult(result,computerSelection);
+				
+			}
+			
+		}
+	}	
 }
